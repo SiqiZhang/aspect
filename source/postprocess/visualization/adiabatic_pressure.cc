ac079eb9cd563fd88f0e9553381f7e65b0d23864
Siqi Zhang <siqi.zhang@mq.edu.au>
2015-08-11 16:57:52 +1000

    Add visualization for static adiabatic pressure (which used to calculate properties for incompressible models)

---
+++ source/postprocess/visualization/adiabatic_pressure.cc
@@ -0,0 +1,84 @@
+/*
+  Copyright (C) 2011 - 2015 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file doc/COPYING.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/postprocess/visualization/adiabatic_pressure.h>
+#include <aspect/simulator_access.h>
+
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      template <int dim>
+      AdiabaticPressure<dim>::
+      AdiabaticPressure ()
+        :
+        DataPostprocessorScalar<dim> ("adiabatic_pressure",
+                                      update_values | update_q_points)
+      {}
+
+
+
+      template <int dim>
+      void
+      AdiabaticPressure<dim>::
+      compute_derived_quantities_vector (const std::vector<Vector<double> >              &uh,
+                                         const std::vector<std::vector<Tensor<1,dim> > > &,
+                                         const std::vector<std::vector<Tensor<2,dim> > > &,
+                                         const std::vector<Point<dim> > &,
+                                         const std::vector<Point<dim> >                  &evaluation_points,
+                                         std::vector<Vector<double> >                    &computed_quantities) const
+      {
+        const unsigned int n_quadrature_points = uh.size();
+        Assert (computed_quantities.size() == n_quadrature_points,    ExcInternalError());
+        Assert (computed_quantities[0].size() == 1,                   ExcInternalError());
+        Assert (uh[0].size() == this->introspection().n_components, ExcInternalError());
+
+        for (unsigned int q=0; q<n_quadrature_points; ++q)
+          {
+            const double pressure    = uh[q][this->introspection().component_indices.pressure];
+
+            computed_quantities[q](0) = this->get_adiabatic_conditions().pressure(evaluation_points[q]);
+          }
+      }
+    }
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      ASPECT_REGISTER_VISUALIZATION_POSTPROCESSOR(AdiabaticPressure,
+                                                  "adiabatic pressure",
+                                                  "A visualization output object that generates output "
+                                                  "for the static adiabatic pressure. "
+                                                  " (The pressure used for calculating properties in incompressible models)")
+    }
+  }
+}
